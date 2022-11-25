af3e96ffa4f4460da0de2401b22ad52e2e9a700a
Kaweesi Matia <matiakaweesi@gmail.com>
2022-11-25 17:03:43 +0300

    solve linter error

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
